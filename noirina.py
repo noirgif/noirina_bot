dd94319fad5a73902134fead31b84c6404f627e1
noirgif <nomaru@outlook.com>
2018-02-20 11:21:38 +0800

    add token

--- noirina.py
+++ noirina.py
@@ -3,10 +3,10 @@ from telegram.ext import Updater
 from telegram.ext import CommandHandler
 import QuoteHandler
 
-token = open('token.sav').readline()
+TOKEN = open('token.sav').readline()
 
-bot = telegram.Bot(token=)
-updater = Updater(token=)
+bot = telegram.Bot(token=TOKEN)
+updater = Updater(token=TOKEN)
 
 lookup_handler = CommandHandler('lookup', QuoteHandler.lookup, pass_args=True)
 
